64f302698f8722e407b72163004573f547d4b11c
manuolm <sample@email>
2024-03-07 19:00:41 +0100

    Archivos Main y CCuenta refactorizados, documentados e implementados en un proyecto Netbeans

---
+++ Tarea4Endes23/src/main/java/entidadFinanciera/CCuenta.java
@@ -0,0 +1,159 @@
+
+package entidadFinanciera;
+
+/**
+ * Objeto CCuenta: contiene información sobre una cuenta bancaria.
+ * 
+ * @author Manuel Olmos Ciari
+ * @version 2
+ */
+public class CCuenta {
+
+    /**
+     * Nombre de la persona titular
+     */
+    private String nombre;
+    
+    /**
+     * Código alfanumerico de la cuenta
+     */
+    private String cuenta;
+    
+    /**
+     * Saldo restante en la cuenta
+     */
+    private double saldo;
+    
+    /**
+     * Usos de la cuenta especificados por el titular
+     */
+    private double tipoInterés;
+    
+    /**
+     * Valor constante usado para errores
+     */
+    private final String ERRORCANTIDADNEGATIVA = "No se puede ingresar una cantidad negativa";
+
+    /**
+     * Constructor por defecto
+     */
+    public CCuenta()
+    {
+    }
+
+    /**
+     * Constructor con tres parámetros
+     * 
+     * @param nom
+     * @param cue
+     * @param sal
+     * @param tipo
+     */
+    public CCuenta(String nom, String cue, double sal, double tipo)
+    {
+        nombre =nom;
+        cuenta=cue;
+        saldo=sal;
+    }
+    
+    /**
+     * Usa el getter getSaldo() para devolver el valor del campo saldo del objeto CCuenta
+     * @return
+     * @see getSaldo
+     */
+    public double estado(){
+        return this.getSaldo();
+    }
+
+    /**
+     * Añade la cantidad especificada como argumento al campo saldo del objeto CCuenta
+     * 
+     * @param cantidad
+     * @throws Exception
+     */
+    public void ingresar(double cantidad) throws Exception
+    {
+        if (cantidad<0)
+            throw new Exception(getERRORCANTIDADNEGATIVA());
+        setSaldo(getSaldo() + cantidad);
+    }
+
+    /**
+     * Quita del campo saldo el valor 'cantidad' especificado como argumento
+     * 
+     * @param cantidad
+     * @throws Exception
+     */
+    public void retirar(double cantidad) throws Exception
+    {
+        if (cantidad <= 0)
+            throw new Exception ("No se puede retirar una cantidad negativa");
+        if (getSaldo() < cantidad)
+            throw new Exception ("No se hay suficiente saldo");
+        setSaldo(getSaldo() - cantidad);
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the cuenta
+     */
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    /**
+     * @param cuenta the cuenta to set
+     */
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    /**
+     * @return the saldo
+     */
+    public double getSaldo() {
+        return saldo;
+    }
+
+    /**
+     * @param saldo the saldo to set
+     */
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    /**
+     * @return the tipoInterés
+     */
+    public double getTipoInterés() {
+        return tipoInterés;
+    }
+
+    /**
+     * @param tipoInterés the tipoInterés to set
+     */
+    public void setTipoInterés(double tipoInterés) {
+        this.tipoInterés = tipoInterés;
+    }
+
+    /**
+     * @return the ERRORCANTIDADNEGATIVA
+     */
+    public String getERRORCANTIDADNEGATIVA() {
+        return ERRORCANTIDADNEGATIVA;
+    }
+}
+
---
+++ Tarea4Endes23/src/main/java/entidadFinanciera/Main.java
@@ -0,0 +1,66 @@
+
+package entidadFinanciera;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Clase principal
+ * 
+ * @author Manuel Olmos Ciari
+ */
+public class Main {
+
+    /**
+     * Método main
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        CCuenta objetoCuenta;
+        double saldoActual;
+        
+
+        objetoCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = objetoCuenta.estado();
+        System.out.println("El saldo actual es"+ saldoActual );
+        
+        try {
+            probarIngresar(objetoCuenta, 2000, 0);
+        } catch (Exception ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        try {
+            probarRetirar(objetoCuenta, 50, 0);
+        } catch (Exception ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    
+    }
+    
+    /**
+     * Intenta ingresar al objeto cuenta especificado la cantidad especificada
+     * 
+     * @param c
+     * @param cantidad
+     * @param cantidadEsperada
+     * @throws Exception
+     */
+    public static void probarIngresar(CCuenta c, int cantidad, int cantidadEsperada) throws Exception{
+        c.ingresar(cantidad);
+        
+    }
+    
+    /**
+     * Intenta retirar del objeto cuenta especificado la cantidad especificada
+     * 
+     * @param c
+     * @param cantidad
+     * @param cantidadEsperada
+     * @throws Exception
+     */
+    public static void probarRetirar(CCuenta c, int cantidad, int cantidadEsperada) throws Exception{
+        c.retirar(cantidad);
+    }
+}
